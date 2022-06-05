467e93b8573f31473ca63f1b80071261afc88a13
Safa ÇAKI <safacaki@gmail.com>
2022-06-05 20:34:57 +0300

    Unused packages and functions deleted

--- src/Helbiz.Application/ApplicationRegistration.cs
+++ src/Helbiz.Application/ApplicationRegistration.cs
@@ -1,6 +1,4 @@
-﻿using MediatR;
-using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Helbiz.Application.Helpers;
 using Helbiz.Application.Interfaces.Helpers;
 using Helbiz.Application.Interfaces.Services;
@@ -13,8 +11,6 @@ namespace Helbiz.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            services.AddMediatR(assembly);
             services.AddScoped<IJwtHelper, JwtHelper>();
             services.AddScoped<IAuthService, AuthService>();
             services.AddScoped<IUserService, UserService>();
